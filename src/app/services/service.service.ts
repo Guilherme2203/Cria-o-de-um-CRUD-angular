ea038b0ace5fa1a9a5453a2f52eb48350ea26837
Guilherme2203 <guilherme.2203@hotmail.com>
2020-05-08 09:33:16 -0300

    update changes on U of CRUD

--- src/app/services/service.service.ts
+++ src/app/services/service.service.ts
@@ -25,8 +25,12 @@ export class LocalStorageService {
 
     array.forEach((element, index) => {
       if (element.code === product.code) array.splice(index, 1);
-    });
+    }); 
 
     this.storage.set('products', array);
+  }
+
+  public update(product: any){
+
   }
 }
