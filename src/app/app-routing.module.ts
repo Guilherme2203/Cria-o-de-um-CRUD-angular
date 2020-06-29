2d044db4b4eaf42a75cf6b79a0f9da1fe0e6f937
Guilherme2203 <guilherme.2203@hotmail.com>
2020-06-29 15:25:29 -0300

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,6 @@ import { Routes, RouterModule } from '@angular/router';
 import { ProductComponent } from './product/product.component';
 import { RegisterComponent } from './register/register.component';
 
-
-
 const routes: Routes = [
   {path: 'product', component: ProductComponent},
   {path: 'register', component: RegisterComponent}
